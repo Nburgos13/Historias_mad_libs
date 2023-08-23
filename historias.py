900f0e92cfdd26365e15157f288eadd3965167b1
“Nicolas Burgos” <burgosdanielnicolas@gmail.com>
2023-08-22 21:31:32 -0300

    Historias Locas - Falta el Readme

--- historias.py
+++ historias.py
@@ -1,1 +1,17 @@
-print("Hola Mundo")
+# Mad Libs (Historias locas)
+
+# jugador
+print("Hola, por favor, completa las siguientes consignas")
+adjetivo = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+# Historia
+
+historia = f"¡Programar es tan {adjetivo}!. ¡Siempre me emociona porque me encanta {verbo1} problemas. " \
+           f"Aprende a {verbo2} y alcanza tu {sustantivo_plural}!"
+
+# Resultado
+
+print(historia)
